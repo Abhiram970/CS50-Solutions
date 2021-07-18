34c25f4fe0b7aa1a2c80879c3155d72876de8115
Abhiram970 <abhiramp428@gmail.com>
2021-07-18 19:31:12 +0530

    Testing

--- 7.sql
+++ 7.sql
@@ -1,1 +1,1 @@
-select avg(energy) from songs where artist_id = (select id from artists where name = 'Drake');
+select avg(energy) from songs where artist_id = (select id from artists where name = 'Drakes');
